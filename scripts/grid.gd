0cf0b3eaf272b0911d22bb23cca5f6c351a1c200
blackbrokkoli <kastomat@gmail.com>
2024-04-18 13:12:33 +0200

    finish tutorial 4

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -1,13 +1,48 @@
 extends Node2D
 
 # Grid Variables for Main Game Grid
-@export var  width: int;
-@export var  height: int;
-@export var  x_start: int;
-@export var  y_start: int;
-@export var  offset: int;
+@export var width: int;
+@export var height: int;
+@export var x_start: int;
+@export var y_start: int;
+@export var offset: int;
 
 var all_pieces: Array = [];
+
+var possible_pieces = [
+	preload("res://scenes/blue_piece.tscn"),
+	preload("res://scenes/green_piece.tscn"),
+	preload("res://scenes/orange_piece.tscn"),
+	preload("res://scenes/pink_piece.tscn"),
+	preload("res://scenes/yellow_piece.tscn")
+]
+
+
+func _ready():
+	all_pieces = make_2d_array();
+	spawn_pieces();
+
+
+func spawn_pieces():
+	for i in width:
+		for j in height:
+			var piece = possible_pieces[randi() % possible_pieces.size()].instantiate();
+			var pos = grid_to_pixel(i, j);
+			piece.position = pos;
+			all_pieces[i][j] = piece;
+			add_child(piece);
+
+# Helper Functions
+
+func grid_to_pixel(col, row):
+	var x = x_start + col * offset;
+	var y = y_start + row * -offset;
+	return Vector2(x, y);
+
+func pixel_to_grid(x, y):
+	var col = int((x - x_start) / offset);
+	var row = int((y - y_start) / offset);
+	return Vector2(col, row);
 
 func make_2d_array():
 	var arr = [];
@@ -16,7 +51,3 @@ func make_2d_array():
 		for j in height:
 			arr[i].append(null);
 	return arr;
-
-func _ready():
-	all_pieces = make_2d_array();
-	print(all_pieces)
