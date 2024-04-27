dc03ec2a81bb720fb118af20f4e630760637e2e7
blackbrokkoli <kastomat@gmail.com>
2024-04-27 18:03:53 +0200

    add gdunit test attempt

---
+++ test/scripts/grid_test.gd
@@ -0,0 +1,12 @@
+# GdUnit generated TestSuite
+class_name GridTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source = 'res://scripts/grid.gd'
+
+
+func test_spawn_pieces() -> void:
+	assert_str("1").is_equal("1")
